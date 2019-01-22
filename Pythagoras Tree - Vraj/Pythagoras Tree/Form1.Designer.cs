ec9a2ad608647de2fee20a448f6792d3f93300bb
vraj-patel <35196999+vraj-patel@users.noreply.github.com>
2019-01-22 00:23:01 -0500

    Add files via upload

---
+++ Pythagoras Tree - Vraj/Pythagoras Tree/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Pythagoras_Tree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.EntireScenePicBox = new System.Windows.Forms.PictureBox();
+            this.AnimationTimer = new System.Windows.Forms.Timer(this.components);
+            this.AnimationButton = new System.Windows.Forms.Button();
+            this.WindTrackBar = new System.Windows.Forms.TrackBar();
+            this.WindForceLabel = new System.Windows.Forms.Label();
+            this.TimeOfDayLabel = new System.Windows.Forms.Label();
+            this.WinterRButton = new System.Windows.Forms.RadioButton();
+            this.SpringRButton = new System.Windows.Forms.RadioButton();
+            this.SummerRButton = new System.Windows.Forms.RadioButton();
+            this.FallRButton = new System.Windows.Forms.RadioButton();
+            this.SeasonLabel = new System.Windows.Forms.Label();
+            this.TimeOfDayListBox = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.EntireScenePicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WindTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EntireScenePicBox
+            // 
+            this.EntireScenePicBox.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.EntireScenePicBox.Location = new System.Drawing.Point(182, 18);
+            this.EntireScenePicBox.Name = "EntireScenePicBox";
+            this.EntireScenePicBox.Size = new System.Drawing.Size(790, 450);
+            this.EntireScenePicBox.TabIndex = 0;
+            this.EntireScenePicBox.TabStop = false;
+            // 
+            // AnimationTimer
+            // 
+            this.AnimationTimer.Interval = 90;
+            this.AnimationTimer.Tick += new System.EventHandler(this.AnimationTimer_Tick);
+            // 
+            // AnimationButton
+            // 
+            this.AnimationButton.Location = new System.Drawing.Point(42, 408);
+            this.AnimationButton.Name = "AnimationButton";
+            this.AnimationButton.Size = new System.Drawing.Size(101, 46);
+            this.AnimationButton.TabIndex = 2;
+            this.AnimationButton.Text = "Start/Randomize Animation";
+            this.AnimationButton.UseVisualStyleBackColor = true;
+            this.AnimationButton.Click += new System.EventHandler(this.AnimationButton_Click);
+            // 
+            // WindTrackBar
+            // 
+            this.WindTrackBar.LargeChange = 1;
+            this.WindTrackBar.Location = new System.Drawing.Point(15, 358);
+            this.WindTrackBar.Name = "WindTrackBar";
+            this.WindTrackBar.Size = new System.Drawing.Size(149, 45);
+            this.WindTrackBar.TabIndex = 3;
+            this.WindTrackBar.Scroll += new System.EventHandler(this.WindTrackBar_Scroll);
+            // 
+            // WindForceLabel
+            // 
+            this.WindForceLabel.AutoSize = true;
+            this.WindForceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WindForceLabel.Location = new System.Drawing.Point(46, 329);
+            this.WindForceLabel.Name = "WindForceLabel";
+            this.WindForceLabel.Size = new System.Drawing.Size(90, 20);
+            this.WindForceLabel.TabIndex = 4;
+            this.WindForceLabel.Text = "Wind Force";
+            // 
+            // TimeOfDayLabel
+            // 
+            this.TimeOfDayLabel.AutoSize = true;
+            this.TimeOfDayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeOfDayLabel.Location = new System.Drawing.Point(40, 94);
+            this.TimeOfDayLabel.Name = "TimeOfDayLabel";
+            this.TimeOfDayLabel.Size = new System.Drawing.Size(93, 20);
+            this.TimeOfDayLabel.TabIndex = 5;
+            this.TimeOfDayLabel.Text = "Time of Day";
+            // 
+            // WinterRButton
+            // 
+            this.WinterRButton.AutoSize = true;
+            this.WinterRButton.Location = new System.Drawing.Point(59, 216);
+            this.WinterRButton.Name = "WinterRButton";
+            this.WinterRButton.Size = new System.Drawing.Size(56, 17);
+            this.WinterRButton.TabIndex = 8;
+            this.WinterRButton.TabStop = true;
+            this.WinterRButton.Text = "Winter";
+            this.WinterRButton.UseVisualStyleBackColor = true;
+            // 
+            // SpringRButton
+            // 
+            this.SpringRButton.AutoSize = true;
+            this.SpringRButton.Location = new System.Drawing.Point(59, 239);
+            this.SpringRButton.Name = "SpringRButton";
+            this.SpringRButton.Size = new System.Drawing.Size(55, 17);
+            this.SpringRButton.TabIndex = 9;
+            this.SpringRButton.TabStop = true;
+            this.SpringRButton.Text = "Spring";
+            this.SpringRButton.UseVisualStyleBackColor = true;
+            // 
+            // SummerRButton
+            // 
+            this.SummerRButton.AutoSize = true;
+            this.SummerRButton.Location = new System.Drawing.Point(59, 262);
+            this.SummerRButton.Name = "SummerRButton";
+            this.SummerRButton.Size = new System.Drawing.Size(63, 17);
+            this.SummerRButton.TabIndex = 10;
+            this.SummerRButton.TabStop = true;
+            this.SummerRButton.Text = "Summer";
+            this.SummerRButton.UseVisualStyleBackColor = true;
+            // 
+            // FallRButton
+            // 
+            this.FallRButton.AutoSize = true;
+            this.FallRButton.Location = new System.Drawing.Point(59, 285);
+            this.FallRButton.Name = "FallRButton";
+            this.FallRButton.Size = new System.Drawing.Size(41, 17);
+            this.FallRButton.TabIndex = 11;
+            this.FallRButton.TabStop = true;
+            this.FallRButton.Text = "Fall";
+            this.FallRButton.UseVisualStyleBackColor = true;
+            // 
+            // SeasonLabel
+            // 
+            this.SeasonLabel.AutoSize = true;
+            this.SeasonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SeasonLabel.Location = new System.Drawing.Point(54, 178);
+            this.SeasonLabel.Name = "SeasonLabel";
+            this.SeasonLabel.Size = new System.Drawing.Size(64, 20);
+            this.SeasonLabel.TabIndex = 12;
+            this.SeasonLabel.Text = "Season";
+            // 
+            // TimeOfDayListBox
+            // 
+            this.TimeOfDayListBox.FormattingEnabled = true;
+            this.TimeOfDayListBox.Items.AddRange(new object[] {
+            "  Day Time",
+            "  Night Time"});
+            this.TimeOfDayListBox.Location = new System.Drawing.Point(53, 127);
+            this.TimeOfDayListBox.Name = "TimeOfDayListBox";
+            this.TimeOfDayListBox.Size = new System.Drawing.Size(70, 30);
+            this.TimeOfDayListBox.TabIndex = 15;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(22, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(135, 59);
+            this.richTextBox1.TabIndex = 16;
+            this.richTextBox1.Text = "  The Life of Fractal Trees";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 480);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.TimeOfDayListBox);
+            this.Controls.Add(this.SeasonLabel);
+            this.Controls.Add(this.FallRButton);
+            this.Controls.Add(this.SummerRButton);
+            this.Controls.Add(this.SpringRButton);
+            this.Controls.Add(this.WinterRButton);
+            this.Controls.Add(this.TimeOfDayLabel);
+            this.Controls.Add(this.WindForceLabel);
+            this.Controls.Add(this.WindTrackBar);
+            this.Controls.Add(this.AnimationButton);
+            this.Controls.Add(this.EntireScenePicBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.EntireScenePicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WindTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox EntireScenePicBox;
+        private System.Windows.Forms.Timer AnimationTimer;
+        private System.Windows.Forms.Button AnimationButton;
+        private System.Windows.Forms.TrackBar WindTrackBar;
+        private System.Windows.Forms.Label WindForceLabel;
+        private System.Windows.Forms.Label TimeOfDayLabel;
+        private System.Windows.Forms.RadioButton WinterRButton;
+        private System.Windows.Forms.RadioButton SpringRButton;
+        private System.Windows.Forms.RadioButton SummerRButton;
+        private System.Windows.Forms.RadioButton FallRButton;
+        private System.Windows.Forms.Label SeasonLabel;
+        private System.Windows.Forms.ListBox TimeOfDayListBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Pythagoras Tree - Vraj/Pythagoras Tree/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Pythagoras_Tree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        static Bitmap drawSceneBtmp = new Bitmap(790, 450);
+
+        float branchAngle = Convert.ToSingle(-Math.PI / 4); 
+
+        // Graphics and brush specifically for drawing the trees 
+        Graphics drawTreesGraphic = Graphics.FromImage(drawSceneBtmp);
+        SolidBrush brushForTrees = new SolidBrush(Color.Green);
+
+        // Graphics specifically for drawing either sun or moon
+        Graphics drawSunAndMoon = Graphics.FromImage(drawSceneBtmp); 
+        SolidBrush brushForSunAndMoon = new SolidBrush(Color.Orange); 
+
+        /* 0.002 radians is approximately 0.1°. The timer ticks every 10th of a second. Therefore, the default 
+        speed is approximately 1°/second      */
+        float windSpeed = 0.002f;
+
+        /* The Animation Button draws trees of random position and size (both are restricted within a range)
+         * In order to preserve this information (position and size of each tree) during animation, these arrays
+         * are used */
+        int[] storeAllPositions = new int[5];
+        int[] storeAllSizes = new int[5];
+
+        /* These 3 branch/leaf colours represent the colours within each tree, branchColour1 being the most 
+         * outward leaf/branch layer. */
+        Color branchColor1 = Color.Green;
+        Color branchColor2 = Color.OrangeRed;
+        Color branchColor3 = Color.DarkGreen;
+
+
+        // Method in charge of drawing the tree using recursion 
+        private void drawTree(float x1, float y1, float angle, float sideLength, int iterations)
+        {
+            // decide the colour based on the value of iteration - or the level of branches being drawn
+            if (iterations <= 8 && iterations >= 6)
+                brushForTrees.Color = Color.SaddleBrown;
+
+            else if (iterations < 6 && iterations >= 4)
+                brushForTrees.Color = branchColor3;
+
+            else if (iterations < 4 && iterations >= 2)
+                brushForTrees.Color = branchColor2;
+
+            else if (iterations < 2 && iterations > 0)
+                brushForTrees.Color = branchColor1;
+
+
+            drawTreesGraphic.FillRectangle(brushForTrees, 0, -sideLength, sideLength, sideLength);
+            drawTreesGraphic.TranslateTransform(0, -sideLength);
+
+            if (iterations > 0)
+            {
+                // Saves the state of the graphic while drawing only the right squares 
+                GraphicsState gsForRightSquares = drawTreesGraphic.Save();
+
+                float sideLength2 = Convert.ToSingle(Math.Abs(Math.Cos(branchAngle) * sideLength)),
+                    angle2 = branchAngle;
+
+                drawTreesGraphic.RotateTransform(Convert.ToSingle(180 * (angle2 / Math.PI)));
+                drawTree(0, -sideLength, angle2, sideLength2, iterations - 1);
+
+                drawTreesGraphic.Restore(gsForRightSquares);
+
+                /* Restores (refer to method above) and saves (refer to method below) the graphic to the 
+                previous state */
+
+                // This section draws the left sided branches of the tree
+                GraphicsState gsForLeftSquares = drawTreesGraphic.Save();
+
+                float x3 = Convert.ToSingle(sideLength2 * Math.Cos(-branchAngle)),
+                        y3 = -(Convert.ToSingle(sideLength2 * Math.Sin(-branchAngle))),
+                        sideLength3 = Convert.ToSingle(Math.Abs(Math.Tan(-branchAngle) * sideLength2)),
+                        angle3 = Convert.ToSingle(Math.PI / 2 - (-branchAngle));
+
+                drawTreesGraphic.TranslateTransform(x3, y3);
+                drawTreesGraphic.RotateTransform(Convert.ToSingle(180 * (angle3 / Math.PI)));
+                drawTree(x3, y3, angle3, sideLength3, iterations - 1);
+
+                drawTreesGraphic.Restore(gsForLeftSquares);
+
+            }
+
+        }
+
+        // Deals with any change wihtin the scenery the user makes 
+        private void changeSceneSettings()
+        {
+            if (TimeOfDayListBox.SelectedIndex == 0)
+                EntireScenePicBox.BackColor = Color.LightSkyBlue;
+            else if (TimeOfDayListBox.SelectedIndex == 1)
+                EntireScenePicBox.BackColor = Color.Black;
+
+            if (WinterRButton.Checked)
+            {
+                branchColor1 = Color.White;
+                branchColor2 = Color.White;
+                branchColor3 = Color.White;
+            }
+            else if (SpringRButton.Checked)
+            {
+                branchColor1 = Color.OrangeRed;
+                branchColor2 = Color.Green;
+                branchColor3 = Color.DarkGreen;
+            }
+            else if (FallRButton.Checked)
+            {
+                branchColor1 = Color.DarkOrange;
+                branchColor2 = Color.DarkOrange;
+                branchColor3 = Color.Green;
+            }
+            else if (SummerRButton.Checked)
+            {
+                branchColor1 = Color.LightGreen;
+                branchColor2 = Color.Green;
+                branchColor3 = Color.Green;
+            }
+
+        }
+
+        // Draws the moon
+        private void drawMoon(int posX, int posY)
+        {
+            brushForSunAndMoon.Color = Color.LightGray;
+            drawSunAndMoon.FillEllipse(brushForSunAndMoon, posX - 75, posY - 75, 150, 150);
+        }
+
+        // Draws the sun
+        private void drawSun(int posX, int posY)
+        {
+            brushForSunAndMoon.Color = Color.Orange;
+
+            // Saves and restores the graphic state for the next method call. 
+
+            GraphicsState gsForSun = drawSunAndMoon.Save();
+            drawSunAndMoon.TranslateTransform(posX, posY);
+
+
+            Point[] pointsForSun = { new Point(0 - 20, 0), new Point(0 + 20, 0), new Point(0, 0 + 75) };
+
+            // Draws the sun by drawing triangles and rotating them 
+            for (int counter = 1; counter <= 72; counter++)
+            {
+                drawSunAndMoon.FillPolygon(brushForSunAndMoon, pointsForSun);
+                drawSunAndMoon.RotateTransform(5f);
+            }
+
+            drawSunAndMoon.Restore(gsForSun);
+
+        }
+
+        // This event randomly chooses the position and size of the trees and saves them within the 2 arrays 
+        private void AnimationButton_Click(object sender, EventArgs e)
+        {
+             
+            Random rndXPosAndSize = new Random();
+            GraphicsState gsOfDrawnTrees = drawTreesGraphic.Save();
+
+            int xPos = 50;
+            int treeSize = 40;
+           
+
+            for (int numOfTrees = 1; numOfTrees <= 5; numOfTrees++)
+            {
+                storeAllPositions[numOfTrees-1] = xPos;
+                storeAllSizes[numOfTrees-1] = treeSize;
+
+                gsOfDrawnTrees = drawTreesGraphic.Save();
+
+                drawTreesGraphic.TranslateTransform(xPos, EntireScenePicBox.Height);
+                drawTree(xPos, EntireScenePicBox.Height, 0, treeSize, 8);
+
+                drawTreesGraphic.Restore(gsOfDrawnTrees);
+
+                xPos += rndXPosAndSize.Next(100, 200);                
+                treeSize += rndXPosAndSize.Next(-5, 12);
+                
+            }
+
+            AnimationTimer.Enabled = true;
+            AnimationTimer.Start(); 
+        }
+
+        // Draws the scenery and changes the angle each time the clock ticks
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+
+            drawTreesGraphic.Clear(EntireScenePicBox.BackColor);
+
+            changeSceneSettings();
+            if (EntireScenePicBox.BackColor == Color.LightSkyBlue)
+                drawSun(100, 100);
+            else if (EntireScenePicBox.BackColor == Color.Black)
+                drawMoon(100, 100); 
+
+
+            GraphicsState gsOverall2 = drawTreesGraphic.Save();
+
+            // Changes windspeed direction if it reaches the following angles: 36° and -36°
+            if (branchAngle >= -Math.PI / 5 || branchAngle <= -Math.PI/2 + Math.PI/5) // change this to change wind speed
+                windSpeed = -windSpeed;
+
+            branchAngle += windSpeed; 
+
+            // draws the identical scenery with position and size store within the array
+            for (int counter = 0; counter < storeAllPositions.Length; counter++)
+            {
+                gsOverall2 = drawTreesGraphic.Save();
+
+                drawTreesGraphic.TranslateTransform(storeAllPositions[counter], EntireScenePicBox.Height);
+                drawTree(storeAllPositions[counter], EntireScenePicBox.Height, 0, storeAllSizes[counter], 8);
+
+                drawTreesGraphic.Restore(gsOverall2);
+            }
+
+            EntireScenePicBox.Image = drawSceneBtmp;
+        }
+
+        // Calculates windspeed depending on the position of WindTrackBar. 
+        private void WindTrackBar_Scroll(object sender, EventArgs e)
+        {
+            windSpeed = (WindTrackBar.Value + 1)*0.002f;
+        }
+
+        
+    }
+}
+
+
